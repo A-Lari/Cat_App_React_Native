a7180d13ffaa73f546a0ef08033eba9287217431
A-Lari <lari.anthony84@gmail.com>
2022-05-19 20:16:32 +0200

    cat app training RN

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [image, setImage] = useState(" ");
+
+  const getCat = () => {
+    fetch("https://api.thecatapi.com/v1/images/search")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setImage(data[0].url);
+        console.log("yes iam the image", image);
+      });
+  };
+
+  useEffect(() => {
+    getCat();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={{ uri: image }} style={styles.image} />
+      <Button onPress={getCat} title="New Cat" color="#841584" />
     </View>
   );
 }
@@ -13,8 +32,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  image: {
+    width: "80%",
+    height: "60%",
+    marginBottom: 20,
   },
 });
